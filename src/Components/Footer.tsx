827312d2e2e6d3dd99adb67b381697498c005861
ahmod001 <ahmodh08@gmail.com>
2024-06-16 11:19:26 +0600

    new: footer added

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer p-4 bg-[#191a1a] text-neutral-content">
+      <p className="mx-auto">
+        <a
+          className="inline"
+          href="https://github.com/ahmod001"
+          target="_blank"
+        >
+          Made with ❤️ by Ahmod Hasan
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
