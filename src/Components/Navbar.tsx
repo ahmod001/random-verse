048a9789beaf6f465d44328844e161452e205861
ahmod001 <ahmodh08@gmail.com>
2024-06-16 11:19:11 +0600

    navbar added

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+
+  return (
+    <div className="navbar bg-[#191a1a] bg-opacity-70 backdrop-blur-md sticky top-0 z-[999]">
+      <div className="flex-1">
+        <NavLink to="/" className="btn btn-ghost text-xl font-semibold">
+          Random Verse
+        </NavLink>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
