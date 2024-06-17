b87ffe8c4f3af9fdc5252964d8594e95de9e8587
ahmod001 <ahmodh08@gmail.com>
2024-06-17 14:07:28 +0600

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['file-saver'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['file-saver'],
-    },
-  },
 })
